7d4e4f3bb16060e06f1f12adb80a9ea145f55660
Arthur Telders <atelders@gmail.com>
2021-07-28 16:54:25 +0200

    Update

--- app/main.py
+++ app/main.py
@@ -2,16 +2,48 @@ from typing import Optional
 
 from fastapi import FastAPI
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 app = FastAPI()
 
-FastAPIInstrumentor.instrument_app(app)
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "diary_api"})
+    )
+)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831
+)
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+
+tracer = trace.get_tracer(__name__)
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    with tracer.start_as_current_span("hello_world"):
+        return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    with tracer.start_as_current_span("get_item"):
+        return {"item_id": item_id, "q": q}
+
+
+with tracer.start_as_current_span("foo"):
+    with tracer.start_as_current_span("bar"):
+        with tracer.start_as_current_span("baz"):
+            print("Hello world from OpenTelemetry Python!")
+
+FastAPIInstrumentor.instrument_app(app)
